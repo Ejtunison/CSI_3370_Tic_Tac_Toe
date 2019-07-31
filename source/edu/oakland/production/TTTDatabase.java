d6374037559be4f62fe67606a82a44b126a364de
brirairigh <47234203+brirairigh@users.noreply.github.com>
2019-07-30 23:04:26 -0400

    Adding code to write to board

--- source/edu/oakland/production/TTTDatabase.java
+++ source/edu/oakland/production/TTTDatabase.java
@@ -13,12 +13,24 @@ public class TTTDatabase {
     would be cell[1][1];     */
 
     // provide method to enter character in a cell;
-    public void writeToBoard(int row, int col, char mark) {
+    public boolean writeToBoard(int row, int col, char mark) {
         // use the helper class "GameBoard.java" for all Board instructions;
+        // check if row and column are in bounds of the board
+        if ((row >= 0) && (row < 3)) {
+            if ((col >= 0) && (col < 3)) {
+                if (board[row][col] == '-') {
+                    board[row][col] = mark;
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
+    
     // provide method to print board after each player move;
     public void printBoardStatus() {
-
+        gameboard.printBoard()
     }
     /* provide a method to return the value of cell[i][j] in order to
     check for a win or a draw;     */
