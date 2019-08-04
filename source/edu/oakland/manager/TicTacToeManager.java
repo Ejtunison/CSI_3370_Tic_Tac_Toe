2a29df2a440a274ecc78fb8f3ab472da32dcafe1
Luke Fox <lmfox@oakland.edu>
2019-08-04 14:15:48 -0400

    Changed TTTDisplay.placeMarkOnBoard to handle all I/O, and removed the Manager's I/O in its selectMove() method

--- source/edu/oakland/manager/TicTacToeManager.java
+++ source/edu/oakland/manager/TicTacToeManager.java
@@ -14,7 +14,7 @@ public class TicTacToeManager
 	private String currentPlayer;
 	private char currentPlayerMark;
 	private boolean isWinOrDrawResult;
-	
+
 	public static void main(String[] args)
 	{
 		//create instance of the TTTManager;
@@ -22,82 +22,68 @@ public class TicTacToeManager
 		database = new TTTDatabase();
 		middleware = new TTTMiddleware(database);
 		display = new TTTDisplay(middleware);
-		
+
 		//launch and initialize all TTTgame clases;
 		manager.launchApp();
-		
+
 		manager.chooseSquare();
 	}
-	
+
 	public void launchApp()
 	{
-		//launch & initialize database, make sure all cells are empty		
+		//launch & initialize database, make sure all cells are empty
 		//launch & initialize middleware, pass database reference
 		//launch & initialize display, pass middleware reference
 
 		display.gameWelcome();
-		
+
 		currentPlayerMark = display.changePlayer('X');
 	}
-	
+
 	//Provide method to select board square
 	public void chooseSquare()
 	{
 		/*Use the scanner class to request the first player's input from the
-		command line, including "Player1" or "Player2", & currentPlayerMark 
-		either 'X' or 'O';*/		
+		command line, including "Player1" or "Player2", & currentPlayerMark
+		either 'X' or 'O';*/
 
 		selectMove();
-		
+
 		checkForWinOrDraw();
 
 		printCurrentBoard();
-		
+
 		chooseSquare();
 	}
-		
+
 	//provide method to choose board move
 	public void selectMove()
 	{
-		System.out.print("\nPlease select a row to place your mark (A/B/C) :: ");
-		Scanner in = new Scanner(System.in);
-		String rowTemp = in.nextLine();
-
-		int rowSelected = 0;
-		
-		if (rowTemp == "A") {rowSelected = 0;}
-		if (rowTemp == "B") {rowSelected = 1;}
-		if (rowTemp == "C") {rowSelected = 2;}
-		
-		System.out.print("\nPlease select a column to place your mark (0/1/2) :: ");
-    	Scanner in2 = new Scanner(System.in);
-    	int columnSelected = in2.nextInt();
-    			
-		display.placeMarkOnBoard(rowSelected, columnSelected, currentPlayerMark);
+		display.placeMarkOnBoard(currentPlayerMark);
 	}
-	
+
 	//Provide method to check for win or draw, if none, changes player
 	public void checkForWinOrDraw()
 	{
 		isWinOrDrawResult = display.checkForWinOrDraw();
-		
+
 		if(!isWinOrDrawResult)
 		{
 			changePlayers();
 		}
 		else
-		{	
+		{
 			launchApp();
 		}
 	}
 
-	
+
 	//provide method to print board status
 	public void printCurrentBoard()
 	{
 		display.printCurrentBoard();
 	}
-	
+
 	//provide method to change players if no win or draw
 	public void changePlayers()
 	{
--- source/edu/oakland/production/TTTDisplay.java
+++ source/edu/oakland/production/TTTDisplay.java
@@ -6,12 +6,9 @@ import java.util.Scanner;
 public class TTTDisplay {
 
     private TTTMiddleware middleware;
-    private int rowNumber;
-    private int columnNumber;
-    private char playerMark;
     private char[][]board;
     private boolean isWinOrDraw;
-    
+
     public TTTDisplay(TTTMiddleware ref) {
         middleware = ref;
     }
@@ -19,9 +16,9 @@ public class TTTDisplay {
     public void gameWelcome() {
 
     	middleware.requestNewGame();
-    	        
+
     	printCurrentBoard();
-    	
+
         Scanner sc = new Scanner(System.in);
         String prompt = "\nWelcome to CSI3370 Summer 2019 Tic Tac Toe!\n\nWould you like to start a new game (yes/no)? :: ";
         String newGame = "";
@@ -30,80 +27,79 @@ public class TTTDisplay {
         System.out.print(prompt);
 
         newGame = sc.nextLine();
-        
+
         newGame = newGame.toLowerCase();
 
         if (! newGame.equals("yes")){
         	System.out.println("\nExiting...\n");
             System.exit(0);
         }
-        
-        System.out.print("\nEnter number of players (1 or 2) :: ");
-        numberOfPlayers = sc.nextInt();
 
-        if (numberOfPlayers == 1) {
-          	System.out.println("\nOne player game against computer feature not currently implemented, select 2 players, please.");
-        }
-          	
-        while (numberOfPlayers != 2) {
-            System.out.println("\nERROR: " + numberOfPlayers + " is an invalid number of players.");
+
+        do {
             System.out.print("\nEnter number of players (1 or 2) :: ");
             numberOfPlayers = sc.nextInt();
-            
+
             if (numberOfPlayers == 1) {
             	System.out.println("\nOne player game against computer feature not currently implemented, select 2 players, please.");
-            }   
-        }        
+            }
+            else {
+                System.out.println("\nERROR: " + numberOfPlayers + " is an invalid number of players.");
+            }
+        }
+        while (numberOfPlayers != 2);
+
+        sc.close();
     }
 
     public void printCurrentBoard() {
 
-        board = middleware.getBoard();        
-                
+        board = middleware.getBoard();
+
         System.out.println();
         System.out.println("+-------------------------------------------------------------+");
-        System.out.println("| ############ CSI3370 Summer 2019 - Tic Tac Toe ############ |");        
+        System.out.println("| ############ CSI3370 Summer 2019 - Tic Tac Toe ############ |");
         System.out.println("+-------------------------------------------------------------+");
         System.out.println("|       Current Board          |        Coordinate Guide      |");
         System.out.println("+-------------------------------------------------------------+");
         System.out.println("|                              |                              |");
-        
-        for(int i = 0; i < 3; i++) 
+
+        for(int i = 0; i < 3; i++)
         {
             System.out.print("|         ");
-                
-            for(int j = 0; j < 3; j++) 
+
+            for(int j = 0; j < 3; j++)
             {
                 if (j != 2)
                 {
                     System.out.print(board[i][j] + " | ");
                 }
- 
-               else 
+
+               else
                 {
                     if (i == 0)
                     {
                         System.out.print(board[i][j] + "            |         A0 | A1 | A2         |");
                     }
-                    
+
                     else if (i == 1)
                     {
                         System.out.print(board[i][j] + "            |         B0 | B1 | B2         |");
                     }
-                    
+
                     else
                     {
                         System.out.print(board[i][j] + "            |         C0 | C1 | C2         |");
                     }
                 }
             }
-        
+
             System.out.println();
 
-            if (i != 2) 
+            if (i != 2)
             {
                 System.out.println("|        -----------           |         --------------       |");
-            }            
+            }
             else
             {
                 System.out.println("|                              |                              |");
@@ -111,24 +107,46 @@ public class TTTDisplay {
 
         }
 
-        System.out.println("+-------------------------------------------------------------+");       
+        System.out.println("+-------------------------------------------------------------+");
     }
 
-    public void placeMarkOnBoard(int row, int column, char mark) {
-        rowNumber = row;
-        columnNumber = column;
-        playerMark = mark;
-        middleware.positionSelected(rowNumber, columnNumber, playerMark);
+    /*
+    Method that prompts the given player to choose a cell to put their mark in.
+    It is the central 'move' function of the game for Display.
+    */
+    public void placeMarkOnBoard(char currentPlayerMark) {
 
-        if (middleware.isValid() == false) {    		
-    		 System.out.println("Invalid move (Mark has already been placed there). Try again - Enter a char for row, 'A', 'B', or 'C' and Enter an int for column, '1', '2', or '3':");
-    		 Scanner in = new Scanner(System.in);
-    		 String input = in.nextLine();
-    		 row = input.charAt(0);
-    		 column = Integer.parseInt("" + input.charAt(1));
-    		
-            placeMarkOnBoard(row, column, playerMark);    		    		
+        System.out.print("\nPlayer " + currentPlayerMark + "\'s Turn: Select an empty cell to place your mark in (Ex. B2) :: ");
+		Scanner scanner = new Scanner(System.in);
+        boolean hasPlacedMark = false;
+        do {
+            String cell = scanner.nextLine().toUpperCase();
+            char rowChar = cell.charAt(0);
+            int rowNumber;
+
+            switch (rowChar) {
+                case 'A': rowNumber = 0;
+                            break;
+                case 'B': rowNumber = 1;
+                            break;
+                case 'C': rowNumber = 2;
+                            break;
+                default:  rowNumber = -1;
+                            break;
+            }
+
+            columnNumber = Character.getNumericValue(cell.charAt(1));
+
+            /* positionSelected returns True if a mark has been placed on the board
+               (input was valid) and False if not (invalid) */
+            hasPlacedMark = middleware.positionSelected(rowNumber, columnNumber, currentPlayerMark);
+            if(!hasPlacedMark) {
+                System.out.println("Error: Mark has already been placed there or invalid input. Try again, Player " + currentPlayerMark + " :: ");
+            }
         }
+        while(!hasPlacedMark);
+
+        scanner.close();
     }
 
     public boolean checkForWinOrDraw() {
